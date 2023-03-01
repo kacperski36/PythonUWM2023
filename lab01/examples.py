f3bc0340a6bd64ea2e5f192a13b8430bf099dd3a
kacperski36 <kacperski36@wp.pl>
2023-03-01 18:18:50 +0100

    first lab

---
+++ lab01/examples.py
@@ -0,0 +1,21 @@
+# zadanie 1
+a = 2  # int
+b = 14  # int
+c = 3.58  # float
+d = 7.79  # float
+
+# zadanie 2
+print(a.bit_count())
+print(b.bit_count())
+
+print(c.is_integer())
+print(d.is_integer())
+
+# zadanie 3
+
+print(a|b)
+print(a^b)
+print(b>>3)
+print(a&b)
+
+
